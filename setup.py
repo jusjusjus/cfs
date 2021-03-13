d6c42a259ed3fc93f1319fc8bd55bf81165ad7ec
jusjusjus <jschwabedal@gmail.com>
2021-03-14 00:51:26 +0100

    Add(gha): linting/testing

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
-
+from os.path import join
 from setuptools import setup
+
+__version__ = ''
+exec(open(join('cfs', 'version.py')).read())
+requirements = open('requirements.txt').read().split('\n')
 
 setup(
     name='cfs',
-    version='0.0.1',
+    version=__version__,
     packages=['cfs'],
+    install_requires=requirements,
     long_description=open('README.md').read(),
-    include_package_data=True
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
 )
