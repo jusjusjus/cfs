78b9b32f97b12a2b2d837daa3795a8de56c7f560
Justus Schwabedal <jschwabedal@gmail.com>
2021-03-20 21:34:38 +0100

    Chg(pkg): to a free name for pypi

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ exec(open(join('cfs', 'version.py')).read())
 requirements = open('requirements.txt').read().split('\n')
 
 setup(
-    name='cfs',
+    name='cleveland-family-study',
     version=__version__,
     packages=['cfs'],
     install_requires=requirements,
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
